9d7363cc10c13ee8b37643b6c9afbbbc43dc1722
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-02-07 21:29:05 +0500

    1

---
+++ MVVM-2/Protocol/CellViewModelType.swift
@@ -0,0 +1,14 @@
+//
+//  CellViewModelType.swift
+//  MVVM-2
+//
+//  Created by Дамир Лутфуллин on 06.02.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+import Foundation
+
+protocol TableViewCellViewModelType: class {
+    var fullName: String { get }
+    var age: String { get }
+}
